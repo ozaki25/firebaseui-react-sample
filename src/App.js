ff15816cfe4f60087c8faae0814d2b2feabf3f61
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-01-29 08:19:55 +0900

    signin and signout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import 'firebase/auth';
 import firebase from 'firebase/app';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-import logo from './logo.svg';
-import './App.css';
-
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
 };
+
 firebase.initializeApp(config);
 
 const uiConfig = {
@@ -18,38 +16,50 @@ const uiConfig = {
   signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
 };
 
+function Login({ signIn }) {
+  useEffect(() => {
+    const unregister = firebase.auth().onAuthStateChanged(signIn);
+    return () => unregister();
+  }, [signIn]);
+
+  return (
+    <div>
+      <h1>Hello！</h1>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
+
+function Home({ user, signOut }) {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>uid: {user.uid}</p>
+      <p>displayName: {user.displayName}</p>
+      <p>email: {user.email}</p>
+      <p>phoneNumber: {user.phoneNumber}</p>
+      <p>photoURL: {user.photoURL}</p>
+      <button onClick={signOut}>SignOut</button>
+    </div>
+  );
+}
+
 function App() {
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
-      console.log({ user });
-      setUser(user);
-    });
-  }, []);
+  const signIn = user => {
+    setUser(user);
+  };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {user && <p>{JSON.stringify(user)}</p>}
-        <StyledFirebaseAuth
-          uiConfig={uiConfig}
-          firebaseAuth={firebase.auth()}
-        />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const signOut = () => {
+    firebase.auth().signOut();
+    setUser(null);
+  };
+
+  return user ? (
+    <Home user={user} signOut={signOut} />
+  ) : (
+    <Login signIn={signIn} />
   );
 }
 
