29c036e15d9b045a325f3c7817814e76f2d86947
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-01-28 18:34:01 +0900

    use react firebaseui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import 'firebase/auth';
+import firebase from 'firebase/app';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
 import logo from './logo.svg';
 import './App.css';
 
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+};
+firebase.initializeApp(config);
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log({ user });
+      setUser(user);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +35,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {user && <p>{JSON.stringify(user)}</p>}
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
