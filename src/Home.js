296bad2e6573aabd51998e6c8acdb8395bf5335f
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-01-29 12:51:21 +0900

    update home

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,12 @@ function Home({ user, signOut }) {
   return (
     <div>
       <h1>Welcome!</h1>
-      <p>uid: {user.uid}</p>
-      <p>displayName: {user.displayName}</p>
-      <p>email: {user.email}</p>
-      <p>phoneNumber: {user.phoneNumber}</p>
-      <p>photoURL: {user.photoURL}</p>
+      <div>
+        <img src={user.photoURL} alt="user icon" width="60" height="60" />
+        <p>名前: {user.displayName}</p>
+        <p>メールアドレス: {user.email}</p>
+        <p>電話番号: {user.phoneNumber || '登録なし'}</p>
+      </div>
       <button onClick={signOut}>SignOut</button>
     </div>
   );
